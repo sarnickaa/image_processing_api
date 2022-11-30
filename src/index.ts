c5b4e2b44a0cddc2370f5e04dd727c5aab5eb35c
Aleksandra Sarnicka <asarnick@its.jnj.com>
2022-11-30 14:19:23 -0500

    initial commit: project structure

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('🎀Hello World!🎀');
+});
+
+app.listen(port, () => {
+  return console.log(`Express is listening at http://localhost:${port}`);
+});
+
+export default app
