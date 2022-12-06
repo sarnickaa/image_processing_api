dc5d21adb92cb72c40e8d4366ecf72093dbd74ca
Aleksandra Sarnicka <asarnick@its.jnj.com>
2022-12-05 23:42:24 -0500

    basic testing

--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -1,11 +1,37 @@
 import supertest from 'supertest';
 import app from '../index';
+import { promises as fsPromises } from 'fs';
+import fs from 'fs';
+import path from 'path'
 
 const request = supertest(app);
+
 describe('Test endpoint responses', () => {
+    beforeAll(function () {
+         fs.copyFileSync(path.resolve(`${__dirname}/../../../src/tests/assets/300-300-cows_test.jpg`), path.resolve(`${__dirname}/../../thumbs/300-300-cows_test.jpg`))
+    });
+
+    afterAll(function () {
+        fs.unlinkSync(path.resolve(`${__dirname}/../../thumbs/300-300-cows_test.jpg`))
+    });
+
+
+
     it('gets the image convert endpoint', async () => {
         const response = await request.get('/api/images');
         console.log(response.status);
+        expect(response.status).toBe(500);
+    })
+
+    it('gets the image convert endpoint', async () => {
+
+        const response = await request.get('/api/images').query({
+            filename: 'cows_test.jpg',
+            width: '300',
+            height: '300'
+        })
+        console.log(response.status);
         expect(response.status).toBe(200);
-    }
-)});
+    })
+
+});
